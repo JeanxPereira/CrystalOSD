2d24c83d4e6b26a0d56d15ae999805d148964eb2
JeanxPereira <jeanlima.vga@gmail.com>
2026-05-03 13:20:21 -0300

    Provide shim standard headers for GCC 2.95 to avoid modern newlib syntax errors

---
+++ include/string.h
@@ -0,0 +1,14 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <stddef.h>
+
+extern void *memcpy(void *dest, const void *src, size_t n);
+extern void *memset(void *s, int c, size_t n);
+extern char *strcpy(char *dest, const char *src);
+extern char *strncpy(char *dest, const char *src, size_t n);
+extern size_t strlen(const char *s);
+extern int strcmp(const char *s1, const char *s2);
+extern int strncmp(const char *s1, const char *s2, size_t n);
+
+#endif
