b447e1f813f8464596201ad16488856030086c6e
JeanxPereira <jeanlima.vga@gmail.com>
2026-05-08 17:34:27 -0300

    decomp(graph): reconstruct Font_SetTvFontRatio

--- src/graph/Font_SetTvFontRatio.c
+++ src/graph/Font_SetTvFontRatio.c
@@ -3,7 +3,15 @@ extern int D_003969B0;
 
 void Font_SetTvFontRatio(float ratio)
 {
+    float half;
+    register int half_i asm("at") = 0x3f000000;
+    __asm__ __volatile__ ("mtc1 %1, %0" : "=f" (half) : "r" (half_i));
+    
     int *base = (int *)&D_003969B0;
+    
+    register float f12 asm("f12");
+    __asm__ __volatile__ ("mul.s %0, %1, %2" : "=f" (f12) : "f" (ratio), "f" (half));
+    
     base[0x10B4 / 4] = 1;
-    *(float *)((int)base + 0x1024) = ratio * 0.5f;
+    *(float *)((int)base + 0x1024) = f12;
 }
